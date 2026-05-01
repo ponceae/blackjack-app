486dcfbb18d652f2e0b25edfcdd54e00017a742c
Adrien P <adrienponce7@gmail.com>
2026-05-01 15:32:19 -0400

    added card images for later

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
 
+app.secret_key = 'some_top_secret_key'
+
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+app.route('/deal')
+def exe_init_deal():
     return render_template('index.html')
 
 if __name__ == '__main__':
